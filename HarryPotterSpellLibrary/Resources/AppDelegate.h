e78ec4104e4b8fa8658128c76407c215e061c6ee
Theo Vora <theo.vora@gmail.com>
2020-03-26 15:25:05 -0600

    initial commit

---
+++ HarryPotterSpellLibrary/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HarryPotterSpellLibrary
+//
+//  Created by theevo on 3/26/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HarryPotterSpellLibrary/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  HarryPotterSpellLibrary
+//
+//  Created by theevo on 3/26/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
